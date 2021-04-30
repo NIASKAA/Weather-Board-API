d45cda4c36c019b45e0b261806bca5c74e70b7bd
NIASKA-Desktop <mkman751@yahoo.com>
2021-04-30 09:10:20 -0300

    fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -195,6 +195,6 @@ window.addEventListener('load', function () {
     .addEventListener('click', getSearchVal);
 });
 
-document.body.style.backgroundImage = "url('../images/background1.jpeg')";
+document.body.style.backgroundImage = "url('/../images/background1.jpeg')";
 document.body.style.backgroundRepeat = "no-repeat";
 document.body.style.backgroundSize = "cover";
